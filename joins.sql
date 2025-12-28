959ba731f815e94fb039755bea206b7223c3b8a9
PraneethAbhiram <komarnenipraneethabhiram@gmail.com>
2025-12-28 20:57:27 +0530

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -1,6 +1,5 @@
 SELECT 
-    s.first_name,
-    c.class_name
-FROM students s
+    s.first_name as Student_Name , c.class_name
+FROM students1 s
 JOIN enrollments e ON s.student_id = e.student_id
-JOIN classes c ON e.class_id = c.class_id;
+JOIN classes1 c ON e.class_id = c.class_id;
